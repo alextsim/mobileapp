e100d297c806405e51faeef53988215209ef684f
z1162156 <alextsiboukis@hotmail.com>
2021-05-07 20:11:23 +1000

    bg and grid view

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,52 +1,57 @@
 import * as React from 'react';
-import { Alert, Button, StyleSheet, TouchableOpacity } from 'react-native';
+import { ImageBackground, StyleSheet, Dimensions } from 'react-native';
+import { View } from '../components/Themed';
 
-import { Text, View } from '../components/Themed';
+const DeviceWidth = Dimensions.get('window').width
 
 export default function TabOneScreen() {
+  
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Beers</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <View>
-      <View style={styles.innerContainer}>
-        <Text
-          style={styles.text}
-          lightColor="rgba(0,0,0,0.8)"
-          darkColor="rgba(255,255,255,0.8)">
-        </Text>
-        {/* print beer list as buttons or images maybe as a table*/}
-        <Button title="CaLton Dry" onPress={() => alert('Simple Button pressed')} />
-      </View>
-    </View>
+    <ImageBackground source={require('../assets/images/bg.jpeg')}  style={styles.container}>
+        <View  style={styles.containerInner}>
+          <View style={{width: DeviceWidth*0.1, height: DeviceWidth*0.1, marginBottom:10, marginLeft:10, backgroundColor: 'black', opacity: 0.8}} />
+          <View style={{width: DeviceWidth*0.1, height: DeviceWidth*0.1, marginBottom:10, marginLeft:10,  backgroundColor: 'black', opacity: 0.8}} />
+          <View style={{width: DeviceWidth*0.1,height: DeviceWidth*0.1, marginBottom:10, marginLeft:10,  backgroundColor: 'black', opacity: 0.8}} />
+        </View>
+        <View  style={styles.containerInner}>
+          <View style={{width: DeviceWidth*0.1, height: DeviceWidth*0.1, marginBottom:10, marginLeft:10,  backgroundColor: 'black',opacity: 0.8}} />
+          <View style={{width: DeviceWidth*0.1, height: DeviceWidth*0.1, marginBottom:10, marginLeft:10,  backgroundColor: 'black',opacity: 0.8}} />
+          <View style={{width: DeviceWidth*0.1, height: DeviceWidth*0.1, marginBottom:10, marginLeft:10,  backgroundColor: 'black', opacity: 0.8}} />
+        </View>
+        <View  style={styles.containerInner}>
+          <View style={{width: DeviceWidth*0.1, height: DeviceWidth*0.1, marginBottom:10, marginLeft:10,  backgroundColor: 'black', opacity: 0.8}} />
+          <View style={{width: DeviceWidth*0.1, height: DeviceWidth*0.1, marginBottom:10, marginLeft:10,  backgroundColor: 'black', opacity: 0.8}} />
+          <View style={{width: DeviceWidth*0.1, height: DeviceWidth*0.1, marginBottom:10, marginLeft:10,  backgroundColor: 'black', opacity: 0.8}} />
+        </View>
+        <View  style={styles.containerInner}>
+          <View style={{width: DeviceWidth*0.1, height: DeviceWidth*0.1, marginBottom:10, marginLeft:10,  backgroundColor: 'black', opacity: 0.8}} />
+          <View style={{width: DeviceWidth*0.1, height: DeviceWidth*0.1, marginBottom:10, marginLeft:10,  backgroundColor: 'black', opacity: 0.8}} />
+          <View style={{width: DeviceWidth*0.1, height: DeviceWidth*0.1, marginBottom:10, marginLeft:10,  backgroundColor: 'black', opacity: 0.8}} />
+        </View>
+      </ImageBackground>
     </View>
   );
 }
 
-//styles are dupicated this should be moved to its own file
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
+    resizeMode: "cover",
+    flexDirection: 'row',
     justifyContent: 'center',
+    alignItems: 'center'
+  },
+    containerInner: {
+    backgroundColor: 'transparent'
+  },
+  bg: {
+    flex: 1,
+    resizeMode: "cover",
+    justifyContent: "center"
   },
   title: {
     fontSize: 20,
     fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
-  innerContainer: {
-    alignItems: 'center',
-    marginHorizontal: 50,
-  },
-  text: {
-    fontSize: 17,
-    lineHeight: 24,
-    textAlign: 'center',
   },
 });
