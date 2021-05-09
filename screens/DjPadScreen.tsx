cacf61f488220f435fae7e59924a1405c428c84b
z1162156 <alextsiboukis@hotmail.com>
2021-05-09 13:38:41 +1000

    dj pad set (play)

--- screens/DjPadScreen.tsx
+++ screens/DjPadScreen.tsx
@@ -1,18 +1,31 @@
 // React imports
 import * as React from 'react';
-import { Button, ImageBackground, StyleSheet, Dimensions } from 'react-native';
+import {ImageBackground, StyleSheet, Dimensions, TouchableHighlight, Text } from 'react-native';
 import { Audio } from 'expo-av';
-
 // project imports
 import { View } from '../components/Themed';
 
 
-// device width varible
-const DeviceWidth = Dimensions.get('window').width
+///////DATA
 
-async function playSound() {
+//grid dimension helper
+const DeviceWidth = Dimensions.get('window').width
+//sounds
+const sources = [
+ "http://www.slspencer.com/Sounds/Chewbacca/Chewie3.mp3",
+ require("../assets/sounds/acousticbassfsharp.mp3"),
+ require("../assets/sounds/ride.mp3"),
+ require("../assets/sounds/bdrum.mp3"),
+ require("../assets/sounds/crash.mp3"),
+ require("../assets/sounds/darkbass.mp3")
+]
+
+///HELPER FUNCTIONS
+
+//audio playing
+async function playSound(index: number) {
   const source = {
-    uri: "http://www.slspencer.com/Sounds/Chewbacca/Chewie3.mp3" 
+    uri: sources[index] 
   } 
   try {
     await Audio.setIsEnabledAsync(true);
@@ -24,43 +37,61 @@ async function playSound() {
   }
 }
 
+///UI
+
 //main
 export default function DJPadScreen() {
+
+
+  //TouchableHighlight: used as buttons, seem to require inner text object
+  
   return (
+    //app container
     <View style={styles.container}>
+      {/* //background */}
     <ImageBackground source={require('../assets/images/bg.jpeg')}  style={styles.container}>
+        {/* COLUMN */}
         <View  style={styles.containerInner}>
-          <View style={styles.box} >
-          <Button title="Chewie we're home" onPress={playSound}/>
-          </View>
-          <View style={styles.box} >
-          <Button title="Chewie we're home" onPress={playSound}/>
-          </View>
-          <View style={styles.box} >
-          <Button title="Chewie we're home" onPress={playSound}/>
-          </View>
+        <TouchableHighlight style={styles.box} onPress={() => playSound(0)}>
+          <Text></Text>
+        </TouchableHighlight>
+        <TouchableHighlight style={styles.box} onPress={() => playSound(1)}>
+          <Text></Text>
+        </TouchableHighlight>
+        <TouchableHighlight style={styles.box} onPress={() => playSound(3)}>
+          <Text></Text>
+        </TouchableHighlight>
         </View>
+         {/* COLUMN */}
         <View  style={styles.containerInner}>
-          <View style={styles.box} />
-          <View style={styles.box} />
-          <View style={styles.box} />
+        <TouchableHighlight style={styles.box} onPress={() => playSound(0)}>
+          <Text></Text>
+        </TouchableHighlight>
+        <TouchableHighlight style={styles.box} onPress={() => playSound(1)}>
+          <Text></Text>
+        </TouchableHighlight>
+        <TouchableHighlight style={styles.box} onPress={() => playSound(3)}>
+          <Text></Text>
+        </TouchableHighlight>
         </View>
+         {/* COLUMN */}
         <View  style={styles.containerInner}>
-          <View style={styles.box} />
-          <View style={styles.box} />
-          <View style={styles.box} />
+        <TouchableHighlight style={styles.box} onPress={() => playSound(0)}>
+          <Text></Text>
+        </TouchableHighlight>
+        <TouchableHighlight style={styles.box} onPress={() => playSound(1)}>
+          <Text></Text>
+        </TouchableHighlight>
+        <TouchableHighlight style={styles.box} onPress={() => playSound(3)}>
+          <Text></Text>
+        </TouchableHighlight>
         </View>
-        <View  style={styles.containerInner}>
-          <View style={styles.box} />
-          <View style={styles.box} />
-          <View style={styles.box} />
-        </View>
-      </ImageBackground>
+        </ImageBackground>
     </View>
   );
 }
 
-//styles
+////STYLING
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -87,6 +118,6 @@ const styles = StyleSheet.create({
     marginBottom:10, 
     marginLeft:10, 
     backgroundColor: 'black', 
-    opacity: 0.6
+    opacity: 0.6,
   }
 });
