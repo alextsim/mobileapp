0d67eb8f149b7b342c2aefdde00820b66b40a002
z1162156 <alextsiboukis@hotmail.com>
2021-05-09 11:55:11 +1000

    chewie we're home (audio can play at button press)

--- screens/DjPadScreen.tsx
+++ screens/DjPadScreen.tsx
@@ -1,6 +1,7 @@
 // React imports
 import * as React from 'react';
-import { ImageBackground, StyleSheet, Dimensions } from 'react-native';
+import { Button, ImageBackground, StyleSheet, Dimensions } from 'react-native';
+import { Audio } from 'expo-av';
 
 // project imports
 import { View } from '../components/Themed';
@@ -9,16 +10,35 @@ import { View } from '../components/Themed';
 // device width varible
 const DeviceWidth = Dimensions.get('window').width
 
+async function playSound() {
+  const source = {
+    uri: "http://www.slspencer.com/Sounds/Chewbacca/Chewie3.mp3" 
+  } 
+  try {
+    await Audio.setIsEnabledAsync(true);
+    const sound = new Audio.Sound();
+    await sound.loadAsync(source);
+    await sound.playAsync(); 
+  } catch(error) {
+    console.error(error);
+  }
+}
+
 //main
 export default function DJPadScreen() {
-  
   return (
     <View style={styles.container}>
     <ImageBackground source={require('../assets/images/bg.jpeg')}  style={styles.container}>
         <View  style={styles.containerInner}>
-          <View style={styles.box} />
-          <View style={styles.box} />
-          <View style={styles.box} />
+          <View style={styles.box} >
+          <Button title="Chewie we're home" onPress={playSound}/>
+          </View>
+          <View style={styles.box} >
+          <Button title="Chewie we're home" onPress={playSound}/>
+          </View>
+          <View style={styles.box} >
+          <Button title="Chewie we're home" onPress={playSound}/>
+          </View>
         </View>
         <View  style={styles.containerInner}>
           <View style={styles.box} />
